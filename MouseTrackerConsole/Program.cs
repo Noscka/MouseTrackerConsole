3e2e8526783e4178b5883073037efce95ea639a3
Ruski1 <adam882200@gmail.com>
2021-12-27 16:18:18 +0000

    Add project files.

---
+++ MouseTrackerConsole/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Timer = System.Timers.Timer;
+using System.Timers;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+using Gma.System.MouseKeyHook;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace MouseTrackerConsole
+{
+    class Program
+    {
+        private static List<Coords> QuickCoords = new List<Coords>();
+        private static int fileNumber = 0;
+        private static Rectangle screenResolution = Screen.PrimaryScreen.Bounds;
+        private static IKeyboardMouseEvents HookEvents = null;
+        private static Timer loopTimer;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo("Spray");
+
+            try
+            {
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+
+            }
+
+            loopTimer = new Timer();
+            loopTimer.Interval = 10;
+            loopTimer.Enabled = false;
+            loopTimer.Elapsed += loopTimerEvent;
+            loopTimer.AutoReset = true;
+
+
+            if (!Directory.Exists("Spray"))
+            {
+                Directory.CreateDirectory("Spray");
+            }
+
+            Hook.GlobalEvents().MouseDown += async (sender, e) => /// Checks for mouse down input (checks for any mouse button)
+            {
+                if (e.Button == MouseButtons.Left) /// checks if the mouse down is left mouse
+                {
+                    loopTimer.Enabled = true; ///enables a loop that records the mouse Co ords
+                }
+            };
+
+            Hook.GlobalEvents().MouseUp += async (sender, e) => /// Checks for mouse up input (checks for any mouse button)
+            {
+                if (e.Button == MouseButtons.Left) /// checks if the mouse up is left mouse
+                {
+                    loopTimer.Enabled = false; ///disables the loop
+                    if (QuickCoords.Count <= 10) /// if the coords are less then 10 then it doesnt write it into file as it is most likely a click
+                    {
+
+                    }
+                    else 
+                    {
+                        using (StreamWriter file = File.CreateText($@"Spray\Spray{fileNumber}.spray")) /// sets up the writing to file which, filenumber is used to be able to store many files
+                        {
+                            JsonSerializer serializer = new JsonSerializer(); /// serialized to json
+                            serializer.Serialize(file, QuickCoords); /// writes the list to the spray file using json format
+                        }
+                        QuickCoords.Clear(); /// clears the list
+                        fileNumber += 1; 
+                    }
+                    
+                }
+            };
+            try
+            {
+                Application.Run(new ApplicationContext());
+            }
+            catch (AccessViolationException)
+            {
+                Environment.Exit(0);
+            }
+        }
+        private static void loopTimerEvent(Object source, ElapsedEventArgs e) /// the loop that records the mouse movement
+        {
+            Console.WriteLine("x: " + Cursor.Position.X + " y: " + Cursor.Position.Y); /// shows the coords in console for debug
+            QuickCoords.Add(new Coords { X = Cursor.Position.X.ToString(), Y = Cursor.Position.Y.ToString() }); /// adds the coords to the list
+        }
+    }
+
+    internal class Coords /// object for the list
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+    }
+}
---
+++ SprayReader/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using Newtonsoft.Json;
+using System.Drawing;
+
+namespace SprayReader
+{
+    class Program
+    {
+        private static List<Coords> QuickCoords = new List<Coords>();
+        static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo("SprayImage");
+            try
+            {
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+            }
+            catch (DirectoryNotFoundException)
+            {
+
+            }
+
+            if (!Directory.Exists("SprayImage"))
+            {
+                Directory.CreateDirectory("SprayImage");
+            }
+
+            string DirectoryOfFiles = args[0];
+            try 
+            {
+                int number = 0;
+                foreach (string file in Directory.GetFiles(DirectoryOfFiles, ".spray"))
+                {
+                    QuickCoords = JsonConvert.DeserializeObject<List<Coords>>(File.ReadAllText(file));
+                    DrawingSpray(QuickCoords, number);
+                    QuickCoords.Clear();
+                    number++;
+                }
+                
+
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Directory Not Found");
+                Console.WriteLine(DirectoryOfFiles);
+                Thread.Sleep(-1);
+            }
+            Thread.Sleep(-1);
+        }
+
+        private static void DrawingSpray(List<Coords> LinesCoords, int filenumber)
+        {
+            int ResolutionX = Screen.PrimaryScreen.Bounds.Width;
+            int ResolutionY = Screen.PrimaryScreen.Bounds.Height;
+
+
+            Bitmap bmp = new Bitmap(ResolutionX, ResolutionY);
+            Pen blackPen = new Pen(Color.Black, 3);
+
+            using (var graphics = Graphics.FromImage(bmp))
+            {
+                graphics.FillRectangle(Brushes.White, new Rectangle(0, 0, ResolutionX, ResolutionY));
+                int index = 0;
+                foreach (var coord in LinesCoords)
+                {
+                    if (index == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        graphics.DrawLine(blackPen, float.Parse(LinesCoords[index-1].X), float.Parse(LinesCoords[index-1].Y), float.Parse(coord.X), float.Parse(coord.Y));
+                    }
+                    index++;
+                }
+            }
+
+            bmp.Save($@"SprayImage\Spray{filenumber}.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+    }
+    internal class Coords
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+    }
+}
